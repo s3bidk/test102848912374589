5daaf52c1a1ece3f7b347bc4f2ffa94b26412cc7
s3bidk <145915221+s3bidk@users.noreply.github.com>
2024-06-11 14:07:00 +0200

    Add files via upload

---
+++ kid.bat
@@ -0,0 +1,62 @@
+cd /d %~dp0
+CupFixerx64.exe /BM "Micro-Star International Co., Ltd."
+CupFixerx64.exe  /BM "Micro-Star International Co., Ltd."
+CupFixerx64.exe /BV 1.0
+CupFixerx64.exe /BS K24327%random%
+CupFixerx64.exe  /BS K24327%random%
+CupFixerx64.exe  /BP "Z390-A PRO (MS-7B98)"
+CupFixerx64.exe /SF "Default string"
+CupFixerx64.exe  /SK "Default string"
+CupFixerx64.exe /SS "Default string"
+CupFixerx64.exe /SV 1.0
+CupFixerx64.exe /SP MS-7B%random%
+CupFixerx64.exe /ID 07/06/2019
+CupFixerx64.exe /IV 1.80
+CupFixerx64.exe  /IVN "American Megatrends Inc."
+CupFixerx64.exe  /SM "Micro-Star International Co., Ltd."
+CupFixerx64.exe /SP MS-7B%random%
+CupFixerx64.exe /ID 07/06/2019
+CupFixerx64.exe /SM "Micro-Star International Co., Ltd."
+CupFixerx64.exe /SP MS-7B%random%
+CupFixerx64.exe  /SP MS-7B%random%
+CupFixerx64.exe /SV 1.0
+CupFixerx64.exe /SS "Default string"
+CupFixerx64.exe  /SS "Default string"
+CupFixerx64.exe /SU auto
+CupFixerx64.exe /SK "Default string"
+CupFixerx64.exe /SF "Default string"
+CupFixerx64.exe /BM "Micro-Star International Co., Ltd."
+CupFixerx64.exe /BP "Z390-A PRO (MS-7B%random%)"
+CupFixerx64.exe /BS K24327%random%
+CupFixerx64.exe /BT "Default string"
+CupFixerx64.exe /BLC "Default string"
+CupFixerx64.exe /CM "Micro-Star International Co., Ltd."
+CupFixerx64.exe  /CM "Micro-Star International Co., Ltd."
+CupFixerx64.exe /CV 0.0
+CupFixerx64.exe /CS "Default string"
+CupFixerx64.exe /CA "Default string"
+CupFixerx64.exe /CO 00000000
+CupFixerx64.exe /CH 00
+CupFixerx64.exe /CPC 01
+CupFixerx64.exe  /CPC 01
+CupFixerx64.exe /CSK "Default string"
+CupFixerx64.exe  /CSK "Default string"
+CupFixerx64.exe /PSN "To Be Filled By O.E.M."
+CupFixerx64.exe /PAT "To Be Filled By O.E.M."
+CupFixerx64.exe /PPN "To Be Filled By O.E.M."
+CupFixerx32.exe /IVN "American Megatrends Inc."
+CupFixerx32.exe  /IVN "American Megatrends Inc."
+CupFixerx32.exe /IV 1.80
+CupFixerx32.exe /ID 04/02/2019
+CupFixerx32.exe /SM "Micro-Star International Co., Ltd."
+CupFixerx32.exe /SP MS-7B%random%
+CupFixerx32.exe /SS "Default string"
+CupFixerx32.exe /SU auto
+CupFixerx32.exe /SF "Default string"
+CupFixerx32.exe /BM "Micro-Star International Co., Ltd."
+CupFixerx32.exe /CV 6.7
+CupFixerx32.exe /CS "Default string"
+CupFixerx32.exe /CA "Default string"
+CupFixerx32.exe /CO 00000000
+CupFixerx32.exe /CPC 01
+CupFixerx32.exe /CSK "Default string"
